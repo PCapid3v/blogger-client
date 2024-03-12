77432d0d746304f8b6492c6cd561583b8a7cdf91
Pierluigi <pierluigi.capirci89@gmail.com>
2024-03-12 17:02:26 +0000

    add: header component and styling

---
+++ src/components/Home/Header/Header.jsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+const Header = () => (
+  <header className="home-header">
+    <h2>PCapid3v Blog</h2>
+    <h1>
+      <span>“</span> Blog <span>”</span>
+    </h1>
+    <p>awesome place to share your ideas </p>
+  </header>
+);
+
+export default Header;
